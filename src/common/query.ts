e751343e31595fa19264ba13dff157f1dad07b78
Njini1 <dbwls6627@gmail.com>
2021-11-18 15:06:42 +0900

    CREATE Users, Posts, Comments Table & DB Connection

--- src/common/query.ts
+++ src/common/query.ts
@@ -16,24 +16,24 @@ export class userQueryString {
 export class postQueryString {
 	public static readonly findAll = "SELECT * FROM `Capstone2021`.`Posts`;";
 	public static readonly findOne =
-		"SELECT * FROM `Capstone2021`.`Posts` WHERE `idx`=?;";
+		"SELECT * FROM `Capstone2021`.`Posts` WHERE `_id`=?;";
 	public static readonly updateOne =
-		"UPDATE `Capstone2021`.`Posts` SET `user_id` = ? `postContent` = ? WHERE (`idx` = ?);";
+		"UPDATE `Capstone2021`.`Posts` SET `user_id` = ? `postContent` = ? WHERE (`_id` = ?);";
 	public static readonly createOne =
 		"INSERT INTO `Capstone2021`.`Posts` (`user_id`, `postContent`) VALUES (?, ?);";
 	public static readonly deleteOne =
-		"DELETE FROM `Capstone2021`.`Posts` WHERE (`idx` = ?);";
+		"DELETE FROM `Capstone2021`.`Posts` WHERE (`_id` = ?);";
 }
 
 export class commentQueryString {
 	public static readonly findAll =
 		"SELECT * FROM `Capstone2021`.`Comments` WHERE `post_id`=?;"; // 한 개의 포스터에 모든 댓글
 	// public static readonly findOne =
-	// 	"SELECT * FROM `Capstone2021`.`Comments ` WHERE `idx`=?;";
+	// 	"SELECT * FROM `Capstone2021`.`Comments ` WHERE `_id`=?;";
 	public static readonly updateOne =
-		"UPDATE `Capstone2021`.`Comments` SET `user_id` = ?, `post_id` = ?, `commentText` = ? WHERE (`idx` = ?);";
+		"UPDATE `Capstone2021`.`Comments` SET `user_id` = ?, `post_id` = ?, `comment_id` = ?, `commentText` = ? WHERE (`_id` = ?);";
 	public static readonly createOne =
-		"INSERT INTO `Capstone2021`.`Comments` (`user_id`, `post_id`, `commentText`) VALUES (?, ?, ?);";
+		"INSERT INTO `Capstone2021`.`Comments` (`user_id`, `post_id`, `comment_id`, `commentText`) VALUES (?, ?, ?, ?);";
 	public static readonly deleteOne =
-		"DELETE FROM `Capstone2021`.`Comments` WHERE (`idx` = ?);";
+		"DELETE FROM `Capstone2021`.`Comments` WHERE (`_id` = ?);";
 }
